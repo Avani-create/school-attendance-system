9619b8a2911e93e12f722134362dc81fd63c4bcb
Avani A <avaniaushas2006@gmail.com>
2026-07-01 07:37:08 +0530

    Force PostgreSQL connection in database.py

--- backend_code/database.py
+++ backend_code/database.py
@@ -1,3 +1,4 @@
+# FORCE POSTGRESQL - NEON DATABASE - DO NOT USE SQLITE
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
